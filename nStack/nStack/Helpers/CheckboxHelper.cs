bab354ac63ed1a051fe874ffe47a6a7ddf468e9f
jmcpherson123 <mcphersonjasmin@gmail.com>
2018-04-26 20:43:14 -0400

    More complex sample program

---
+++ nStack/nStack/Helpers/CheckboxHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nStack.Helpers
+{
+    public class CheckboxHelper
+    {
+        public string Name { get; set; }
+        public bool Checked {get; set;}
+    }
+}
---
+++ nStack/nStack/ViewModels/UploadViewModel.cs
@@ -0,0 +1,20 @@
+﻿using nStack.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace nStack.Models
+{
+    public class UploadViewModel:BinFileObject
+    {
+        public BinFileObject ReadData;
+        public Dictionary<string, List<List<string>>> sortedData;
+        public string section;
+
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+    }
+}
