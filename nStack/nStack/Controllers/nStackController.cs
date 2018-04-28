fc1a707f1af63007aa707bcb392e5665381d06ae
jmcpherson123 <mcphersonjasmin@gmail.com>
2018-04-28 08:06:03 -0400

    added in working program attemp 1

---
+++ nStack/nStack/Controllers/nStackController.cs
@@ -0,0 +1,52 @@
+﻿using nStack.Helpers;
+using nStack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nStack.Controllers
+{
+    public class nStackController : Controller
+    {
+        // GET: nStack
+
+        public ActionResult nStack()
+        {
+            //Read Data start here
+            nStackViewModel vm = new nStackViewModel();
+            vm = nStackHelper.setUpnStackWithoutSelection();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult nStack(nStackViewModel vm)
+        {
+            vm = nStackHelper.setUpnStackWithSelection(vm);
+            return View(vm);
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase[] file, UploadViewModel vm)
+        {
+            FileReader fileReader = new FileReader();
+            if (file.Count() > 1)
+            {
+                var compName = vm.CompanyName.Split(',').ToList();
+                int i = 0;
+                foreach (var fileUploaded in file)
+                {
+                    vm.ReadData = fileReader.getBinFileObject(fileUploaded, compName.ElementAt(i));
+                    SaveData saveObject = new SaveData(vm.ReadData);
+                    i++;
+                }
+            }
+            else
+            {
+                vm.ReadData = fileReader.getBinFileObject(file[0], vm.CompanyName);
+                SaveData saveObject = new SaveData(vm.ReadData);
+            }
+            return View(vm);
+        }
+    }
+}
